fc8384cd22b2484ee5395bead783f8cf2aa054e2
muhammadIdhamMaarif <muhammadidhammaarif@gmail.com>
2024-10-11 07:29:59 +0700

    Create Harga_Tiket_Wahana.cpp

---
+++ Harga_Tiket_Wahana.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int umur;
+	cin >> umur;
+	if (umur < 13) {
+		cout << "Rp 15.000";
+	}
+	else if (umur >= 13 && umur <= 17) {
+		cout << "Rp 25.000";
+	}
+	else {
+		cout << "Rp 25.000";
+	}
+	return 0;
+}
